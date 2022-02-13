2254fabf3661f9df5bc4cc03375fa19847aede26
sunnyrocks1122 <sunnyrocks1122@gmail.com>
2022-02-13 20:12:28 +0530

    Added Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, NgbModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
