5c60755e8d837f2f8b9edc42bf7d202f1c85fac7
sunnyrocks1122 <sunnyrocks1122@gmail.com>
2022-02-13 20:26:55 +0530

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
+import { HeaderComponent } from './_shared/header/header.component';
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, HeaderComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
