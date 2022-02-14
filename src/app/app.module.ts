cc46f3cb37b67b38ab642c87c142b34236ac061b
sunnyrocks1122 <sunnyrocks1122@gmail.com>
2022-02-14 23:18:43 +0530

    added router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './_shared/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    AppRoutingModule,
-    HelloComponent,
-    HeaderComponent,
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, HeaderComponent, HomeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
