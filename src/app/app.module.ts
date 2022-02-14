dab2f03f9bdbfc0c533046c408639559bc67c71c
sunnyrocks1122 <sunnyrocks1122@gmail.com>
2022-02-14 22:54:05 +0530

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './_shared/header/header.component';
+
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    AppRoutingModule,
+    HelloComponent,
+    HeaderComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
