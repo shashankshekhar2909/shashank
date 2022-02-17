3064d24a72a446b421040789462f9855fb325f06
sunnyrocks1122 <sunnyrocks1122@gmail.com>
2022-02-18 03:20:03 +0530

    7 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
-  { path: 'home', component: HomeComponent, data: { title: 'Home' } },
+  { path: '', component: HomeComponent, data: { title: 'Home' } },
 ];
 
 @NgModule({
