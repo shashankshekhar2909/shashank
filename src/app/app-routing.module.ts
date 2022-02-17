58f312118e1cab36310e8df99f487b8445c4efac
sunnyrocks1122 <sunnyrocks1122@gmail.com>
2022-02-18 00:51:38 +0530

    4 files modified, 7 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
-  { path: '', component: HomeComponent, data: { title: 'Home' } },
+  { path: 'home', component: HomeComponent, data: { title: 'Home' } },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,21 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './_shared/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { PersonalComponent } from './components/personal/personal.component';
+import { ProfessionalComponent } from './components/professional/professional.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   imports: [BrowserModule, AppRoutingModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HeaderComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeaderComponent,
+    HomeComponent,
+    PersonalComponent,
+    ProfessionalComponent,
+    ContactComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
